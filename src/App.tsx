18d43c01d7295b9a174f4a73fb66e83cc2e8c2f4
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-23 12:54:20 +0300

    add chackra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,9 @@
-import { AxiosError } from "./services/apiClient";
-import { userService, User } from "./services/userService";
-import { useUsers } from "./hooks/useUsers";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  const { users, error, isLoading, setError, setUsers } = useUsers();
-
-  const deleteUser = ({ id }: User) => {
-    const origUsers = [...users];
-
-    setUsers(users.filter((user) => user.id !== id));
-
-    // Promise.reject(new AxiosError("error"))
-    userService.delete(id).catch((error) => {
-      setError((error as AxiosError).message);
-      setUsers(origUsers);
-    });
-  };
-
-  const addUser = () => {
-    const origUsers = [...users];
-    const user: User = {
-      id: 0,
-      name: "New User",
-    };
-
-    setUsers([user, ...users]);
-
-    userService
-      .create(user)
-      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
-      .catch((error) => {
-        setError((error as AxiosError).message);
-        setUsers(origUsers);
-      });
-  };
-
-  const updateUser = (user: User) => {
-    const origUsers = [...users];
-    const updatedUser = { ...user, name: `${user.name}!!!` };
-
-    setUsers((users) => users.map((u) => (u.id === user.id ? updatedUser : u)));
-
-    userService.update(updatedUser).catch((error) => {
-      setError((error as AxiosError).message);
-      setUsers(origUsers);
-    });
-  };
-
   return (
     <>
-      {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>
-        Add
-      </button>
-      <ul className="list-group">
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            {user.name}{" "}
-            <div className="d-flex gap-2">
-              <button
-                className="btn btn-outline-secondary"
-                onClick={() => updateUser(user)}
-              >
-                Update
-              </button>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => deleteUser(user)}
-              >
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App.tsx";
 
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );

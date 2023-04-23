2822def8c799297f29e3e1ace9b44b9aa58c8eca
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-23 21:53:23 +0300

    dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App.tsx";
+import { theme } from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>
 );
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+export const theme = extendTheme({
+  config,
+});

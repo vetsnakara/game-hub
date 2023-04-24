bad9497c4690a48d03502cc547aad4042bdc3b2d
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-24 11:15:04 +0300

    get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from "react";
-
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+
 import { Game } from "../hooks/useGames";
+import { getCroppedImageUrl } from "../services/imageUrl";
+
 import { PlatformIconList } from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
 
@@ -16,7 +17,7 @@ export const GameCard = (props: GameCardProps) => {
 
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">

546a2e617c3dc0745693ba7b6b3671802069248d
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2023-04-24 12:30:07 +0300

    spinner for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 
 import { getCroppedImageUrl } from "../services/imageUrl";
 import { useGenres } from "../hooks/useGenres";
 
 export const GenreList = () => {
-  const { data: genres } = useGenres();
+  const { data: genres, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
   return (
     <List>
       {genres.map((genre) => (
